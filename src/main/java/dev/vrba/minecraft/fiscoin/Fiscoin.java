2fa1db8592b676267ef5a55ad0c5adf13dd8e2e0
Jiří Vrba <jiri@vrba.dev>
2021-02-11 11:43:52 +0100

    Initial commit

---
+++ src/main/java/dev/vrba/minecraft/fiscoin/Fiscoin.java
@@ -0,0 +1,15 @@
+package dev.vrba.minecraft.fiscoin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public final class Fiscoin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
